037ee33b70d2ab68390c4dccc5128c714e70ba31
Javier Salcedo <jsalcedo@jaguarlandrover.com>
2024-11-28 15:36:34 +0000

    formatting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,3 @@
-
 fn spawn_local() {
     let server = zero2prod::run().expect("Failed to bind address");
     let _ = tokio::spawn(server);
@@ -8,12 +7,12 @@ fn spawn_local() {
 async fn health_check_works() {
     spawn_local();
     let client = reqwest::Client::new();
-    let response = client.get("http://127.0.0.1:8000/health_check")
-                         .send()
-                         .await
-                         .expect("Failed to execute request.");
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-
 }
