1ce69678fb11a15f607e4f6dfe6ac4d5c7fc25dc
Javier Salcedo <jsalcedo@jaguarlandrover.com>
2024-11-28 14:34:54 +0000

    integration tests working

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Zero2Prod Book practices
 ## Newsletter Email App
+
+
+### Requirements - User Stories
+- As a blog visitor, I want to subscribe to the newsletter, So that I can receive email updates when new content is published on the blog; 
+
+- As the blog author, I want to send an email to all my subscribers, So that I can notify them when new content is published.
---
+++ tests/health_check.rs
@@ -0,0 +1,19 @@
+
+fn spawn_local() {
+    let server = zero2prod::run().expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    spawn_local();
+    let client = reqwest::Client::new();
+    let response = client.get("http://127.0.0.1:8000/health_check")
+                         .send()
+                         .await
+                         .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+
+}
